d78364a84e2ad8851046677898635be02851da3a
jayeshC01 <noreply@email.com>
2025-09-06 11:30:33 +0530

    Implementing XREAD

--- src/main/java/processors/streams/XREADExecutor.java
+++ src/main/java/processors/streams/XREADExecutor.java
@@ -1,4 +1,36 @@
 package processors.streams;
 
-public class XREADExecutor {
+import db.DataStore;
+import models.DataStoreValue;
+import models.RespCommand;
+import processors.CommandExecutor;
+import utility.RespUtility;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentNavigableMap;
+
+public class XREADExecutor implements CommandExecutor {
+
+  @Override
+  public String execute(RespCommand cmd) {
+    List<String> args = cmd.getArgs();
+    String key = args.get(1);
+    String streamId = args.get(2);
+    DataStoreValue value = DataStore.get(key);
+    if(value == null) {
+      return "*-1\r\n";
+    }
+    ConcurrentNavigableMap<String, Map<String, String>> streamData = value.getAsStream();
+    return encodeResponse(streamData.tailMap(streamId, false), key);
+  }
+
+  private String encodeResponse(ConcurrentNavigableMap<String, Map<String, String>> output, String key) {
+    StringBuilder op = new StringBuilder();
+    op.append("*1\r\n*2\r\n");
+    op.append(RespUtility.serializeResponse(key));
+    op.append(RespUtility.serializeStreamResponse(output));
+    System.out.println("oppppp"+ op);
+    return op.toString();
+  }
 }
